faa4b64a408ed35c2fea254149d379d13b034b36
Radha gupta <1radha21@gmail.com>
2025-06-29 11:06:50 +0530

    Added superuser

--- libray_app/views.py
+++ libray_app/views.py
@@ -68,6 +68,12 @@ class requestbook(generics.CreateAPIView):
             serializer.save(borrowed_date=borrowed_date,return_date=return_date)
 
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "adminpassword123")
 
 
 
